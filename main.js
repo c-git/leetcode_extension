afa1730989cf309c94052df2ee654db9309d96d1
One <43485962+c-git@users.noreply.github.com>
2023-10-21 23:59:59 -0400

    Code that produces and acceptable result

--- main.js
+++ main.js
@@ -1,4 +1,54 @@
 /*
 Hide the difficulty if found
 */
-document.body.style.border = "5px solid red";
+
+(() => {
+    console.log("EXTENSION to Hide Difficulty Starting Up");
+    let intervalId;
+    let loopCount = 0;
+    const NUM_SECONDS_TO_TRY = 10;
+    const TIMES_PER_SECOND = 4;
+    const REPEAT_COUNT = NUM_SECONDS_TO_TRY * TIMES_PER_SECOND;
+
+    function hideDifficulty() {
+        loopCount += 1;
+        console.log(`Loop iteration: ${loopCount}`);
+        if (loopCount > REPEAT_COUNT) {
+            // Repeated enough times, it's either done or won't be done
+            clearInterval(intervalId);
+            console.log("Stopping Loops to hide difficulty");
+        }
+
+        let element = document.getElementById("qd-content");
+        function not_found() {
+            document.body.style.border = "5px solid red";
+            throw "Not Found";
+        }
+        if (element === null) { not_found() }
+
+        element = element.children[0];
+        if (element === undefined) { not_found() }
+
+        element = element.children[3];
+        if (element === undefined) { not_found() }
+
+        element = element.children[0];
+        if (element === undefined) { not_found() }
+
+        element = element.children[0];
+        if (element === undefined) { not_found() }
+
+        element = element.children[1];
+        if (element === undefined) { not_found() }
+
+        element = element.children[0];
+        if (element === undefined) { not_found() }
+
+        document.body.style.border = ""; // Clear any error status
+        element.style.color = "black";
+        element.style.backgroundColor = "black";
+        element.style.width = "100px"; // To prevent determination based on width as Hard is shorter than Medium
+    }
+
+    intervalId = setInterval(hideDifficulty, 1000 / TIMES_PER_SECOND);
+})()
